3005193785d4af2808a287ca9e176256c3270723
bkmz2208 <bkmz2208@gmail.com>
2020-10-01 16:48:06 +0300

    Add carMethods

--- src/CarMethods.java
+++ src/CarMethods.java
@@ -1,22 +1,38 @@
 package src;
 
+import java.unit.Calendar;
+import java.unit.GregorianCalendar;
+
+
 public class CarMethods {
-    public sortByMark(List[] ArrayCars, String mark) {
+    public sortByCompany(List<Car> ListOfCars, String company) {
         ArrayList<Car> newListCars = new ArrayList<Car>();
-        for (int i = 0; i < ArrayCars.length; i++) {
-            if (ArrayCars(i).getMark() == mark){
-                newListCars.add(ArrayCars(i));
+        for (Car car : ListOfCars) {
+            if (car.getCompany() == company) {
+                newListCars.add(car));
             }
         }
         return newListCars;
-
     }
 
-    public sortByMarkAndYear() {
-
+    public sortByCompanyAndYear(List<Car> ListOfCars, String company, int countOfYear) {
+        ArrayList<Car> newListCars = new ArrayList<Car>();
+        currentYear = new GregorianCalendar().get(Calendar.YEAR);
+        for (Car car : ListOfCars) {
+            if (car.getCompany() == company && countOfYear > (currentYear - car.getYear())) {
+                newListCars.add(car));
+            }
+        }
+        return newListCars;
     }
 
-    public sortByYearAndPrice() {
-
+    public sortByYearAndPrice(List<Car> ListOfCars, int price, int year) {
+        ArrayList<Car> newListCars = new ArrayList<Car>();
+        for (Car car : ListOfCars) {
+            if (car.getYear() == year && price > car.getPrice()) {
+                newListCars.add(car));
+            }
+        }
+        return newListCars;
     }
 }
